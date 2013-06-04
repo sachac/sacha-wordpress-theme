df64fa582e1c8521722f26702a332f77fd3ea018
Sacha Chua <sacha@sachachua.com>
2013-06-04 19:56:50 -0400

    Tweak footer

--- footer.php
+++ footer.php
@@ -1,20 +1,11 @@
+<br />
 <div class="grid_16 footer">
-  <h2 class="widgettitle">Featured Categories</h2>
-  <ul class="featured_categories">
-    <li class="cat_emacs"><a href="http://sachachua.com/blog/category/emacs"><span>Emacs</span></a></li>
-    <li class="cat_sketches"><a href="http://sachachua.com/blog/category/sketches"><span>Sketches</span></a></li>
-    <li class="cat_books"><a href="http://sachachua.com/blog/category/visual-book-notes"><span>Visual book notes</span></a></li>
-    <li class="cat_geek"><a href="http://sachachua.com/blog/category/geek"><span>Geek</span></a></li>
-    <li class="cat_life"><a href="http://sachachua.com/blog/category/life"><span>Life</span></a></li>
-    <li class="cat_outsourcing"><a href="http://sachachua.com/blog/category/delegation"><span>Delegation</span></a></li>
-    <li class="cat_speaking"><a href="http://sachachua.com/blog/category/presentation"><span>Presenting</span></a></li>
-    <li class="cat_connecting"><a href="http://sachachua.com/blog/category/networking"><span>Social networking</span></a></li>
-    <li class="cat_weekly"><a href="http://sachachua.com/blog/category/weekly"><span>Weekly reports</span></a></li>
-</ul></li>
-</div>
-<div class="clear"></div>
-<div class="grid_14 footer">
-  <p>Like this? <a href="http://feeds.feedburner.com/sachac" onclick="javascript:recordOutboundLink(this, 'Subscription','/subscribe/rss/text');return false;">Subscribe using your feed reader</a> or <a href="http://sachachua.com/blog/subscribe">your e-mail client</a>! <a href="http://twitter.com/sachac">Follow me on Twitter</a> for quick updates, and add me on <a href="https://plus.google.com/113865527017476906160/posts?rel=author" rel="publisher">Google+</a>. <a href="http://sachachua.com/blog/contact">Contact me</a>: I'd love to hear your thoughts, questions, and suggestions!</p>
+
+  <p>Like this? <a href="http://feeds.feedburner.com/sachac" onclick="javascript:recordOutboundLink(this, 'Subscription','/subscribe/rss/text');return false;">
+Subscribe using your feed reader</a> or <a href="http://sachachua.com/blog/subscribe">your e-mail client</a>! 
+<a href="http://twitter.com/sachac">Follow me on Twitter</a> for quick updates, and add me on 
+<a href="https://plus.google.com/113865527017476906160/posts?rel=author" rel="publisher">Google+</a>. <a href="http://sachachua.com/blog/contact">Contact me</a>: 
+I'd love to hear your thoughts, questions, and suggestions!</p>
   <br />
   <ul class="links">
     <li><a href="/blog">Home</a></li>
@@ -30,14 +21,6 @@
   <p>
     Copyright &#169; 2001-2012 Sacha Chua (<a href="mailto:sacha@sachachua.com">sacha@sachachua.com</a>). Please feel free to reuse content under a <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution Non-commercial Sharealike</a> license unless otherwise noted.
   </p>
-</div>
-<div class="grid_2">
-<div class="widget">
-<h2>Get the highlights as a PDF!</h2>
-<p><a href="http://www.lulu.com/shop/sacha-chua/stories-from-my-twenties-highlights-of-a-decade-of-blogging/ebook/product-20223728.html"><img width="100" src="/blog/wp-content/themes/sacha-v3/images/stories-from-my-twenties.png" alt="Stories from my Twenties: Highlights from a Decade of Blogging" /></a></p>
-<p><a href="http://db.tt/fGnql5ij">Free sample!</a></p>
-</div>
-
 </div>
 
 
