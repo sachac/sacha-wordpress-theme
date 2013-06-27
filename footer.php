26bacf1267b28cb4c4f22a5c391664e766f610a2
Sacha Chua <sacha@sachachua.com>
2013-06-27 13:05:07 -0400

    Add chat

--- footer.php
+++ footer.php
@@ -27,6 +27,15 @@ I'd love to hear your thoughts, questions, and suggestions!</p>
 <script src="http://www.google.com/jsapi" type="text/javascript"></script>
 <?php wp_footer(); ?>
 </div>
+<!--Start of Zopim Live Chat Script-->
+<script type="text/javascript">
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+$.src='//v2.zopim.com/?1Khd9ocvrNwRDUHR4pE2Ylr4m8oaXe6t';z.t=+new Date;$.
+type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+</script>
+<!--End of Zopim Live Chat Script-->
 <script type="text/javascript">
 jquery(document).ready(function() {
    jquery('.sparkline').sparkline();
