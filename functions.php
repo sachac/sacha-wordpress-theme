1afbc1c6a2f7be46a1989b106f5ece2da23c4f86
Sacha Chua <sacha@sachachua.com>
2014-02-14 11:06:54 -0500

    Add modify_url

--- functions.php
+++ functions.php
@@ -49,4 +49,39 @@ function random_template() {
                exit;
        }
 }
+
+function modify_url($mod) 
+{ 
+    $url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; 
+    $query = explode("&", $_SERVER['QUERY_STRING']);
+    if (!$_SERVER['QUERY_STRING']) {$queryStart = "?";} else {$queryStart = "&";}
+    // modify/delete data 
+    foreach($query as $q) 
+    { 
+        list($key, $value) = explode("=", $q); 
+        if(array_key_exists($key, $mod)) 
+        { 
+            if($mod[$key]) 
+            { 
+                $url = preg_replace('/'.$key.'='.$value.'/', $key.'='.$mod[$key], $url); 
+            } 
+            else 
+            { 
+                $url = preg_replace('/&?'.$key.'='.$value.'/', '', $url); 
+            } 
+        } 
+    } 
+    // add new data 
+    foreach($mod as $key => $value) 
+    { 
+        if($value && !preg_match('/'.$key.'=/', $url)) 
+        { 
+            $url .= $queryStart.$key.'='.$value; 
+        } 
+    } 
+    $url = preg_replace('/\/&/', '/?', $url);
+    return $url; 
+} 
+
 ?>
+
