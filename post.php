e4d617ad50ed22fa31f93b31fb9aaaa851346253
Sacha Chua <sacha@sachachua.com>
2014-03-10 10:58:51 -0400

    Tweak spacing

--- post.php
+++ post.php
@@ -5,10 +5,10 @@
 
 			<div class="postinfo">
 <span class="postdate"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:s\Z') ?>"><?php the_time('F jS, Y') ?></abbr></span> <?php _e('Categories: '); the_category(', '); ?> <?php the_tags(); ?> 
-<?php 
+| <?php 
 $themeurl = get_stylesheet_directory_uri(); 
    	   $anchor_svg = 'anchor-svg';
-   	   $comment_link = "<object data ='".$themeurl."/images/comment.svg'><img class='anchor-png' alt='' src='".$themeurl."/images/comment.png' /></object>"; 
+$comment_link = "<div class='svg-container' style='width: 1.3em; height: 1.6em; padding-bottom: 0em'><object width='100%' height='100%' data='".$themeurl."/images/comment.svg'><img class='anchor-png' alt='' src='".$themeurl."/images/comment.png' /></object></div>"; 
 	 comments_popup_link($comment_link, '1&nbsp;' . $comment_link ,
                  '% '.$comment_link,$anchor_svg); ?>
 			</div>
