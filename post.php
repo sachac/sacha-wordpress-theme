c1523fb21cf2f0d8b0f906121ebe5ce2ee87fb54
maisnamraju@gmail.com <maisnamraju@gmail.com>
2014-03-10 11:55:12 +0530

    svgs and pngs added, need to decide on the size of both and align them

--- post.php
+++ post.php
@@ -6,9 +6,12 @@
 			<div class="postinfo">
 <span class="postdate"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:s\Z') ?>"><?php the_time('F jS, Y') ?></abbr></span> <?php the_author() ?>  <?php _e('Categories: '); the_category(', '); ?> <?php the_tags(); ?> 
 
-<?php comments_popup_link('Add Comment &#187;', '1 Comment
-                                                            &#187;',
-                 '% Comments &#187;'); ?>
+<?php 
+   	   $themeurl = get_stylesheet_directory_uri(); 
+   	   $anchor_svg = 'anchor-svg';
+   	   $comment_link = "<object data ='".$themeurl."/images/comment.svg'><img class='anchor-png' src ='".$themeurl."/images/comment.png' /></object>"; 
+	 comments_popup_link($comment_link, '1&nbsp;'.$comment_link ,
+                 '% '.$comment_link,$anchor_svg); ?>
 
 			</div>
 			<div class="entry">
