1b0424149f76bc9a942a6e6507306f88f9a8329f
Sacha Chua <sacha@sachachua.com>
2014-03-30 11:18:39 -0400

    First steps towards new footer

--- footer.php
+++ footer.php
@@ -1,40 +1,42 @@
 </div>
 <footer class="row footer">
-<div class="small-12 columns">
+  <div class="small-4 columns">
+    <p><a href="http://sachachua.com/blog/subscribe">Subscribe</a></p>
+    <p>Connect:</p>
+    <a href="http://twitter.com/sachac">Twitter: @sachac</a> 
+  
+    <a href="https://plus.google.com/113865527017476906160/posts?rel=author" rel="publisher">Google+</a><br />
+<a href="http://sachachua.com/blog/contact">E-mail</a>
+  </div><div class="small-4 columns"><p>
+    <ul class="links">
+      <li><a href="/blog">Home</a></li>
+      <li><a href="/blog/about/">About</a></li>
+      <li><a href="http://dl.dropbox.com/u/3968124/blog.html">Index</a></li>
+      <li><a href="/blog/archive/">Archives</a></li>
+      <li><a href="/blog/?random">Random</a></li>
+      <li><a href="/blog/privacy">Privacy</a></li>
+      <li><a href="/blog/contact">Contact</a></li>
+    </ul></p>
+  </div><div class="small-4 columns">
+  <p><iframe src="https://helpouts.google.com/promote/113865527017476906160?k=3" scrolling="no" style="border-style: none; overflow: hidden; width: 184px; height: 154px;">
+  </iframe></p> 
+  or <a href="http://sachachua.com/meet">schedule a meeting</a>
+  </div></div>
+  <div class="row">
+      Copyright &#169; 2001-2014 Sacha Chua (<a href="mailto:sacha@sachachua.com">sacha@sachachua.com</a>). Please feel free to reuse or share content under a <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution</a> license unless otherwise noted. You can generally click on images for larger versions. Thanks for reading!
+  </div>
 
-  <p>Like this? <a href="http://feeds.feedburner.com/sachac" onclick="javascript:recordOutboundLink(this, 'Subscription','/subscribe/rss/text');return false;">
-Subscribe using your feed reader</a> or <a href="http://sachachua.com/blog/subscribe">your e-mail client</a>! 
-<a href="http://twitter.com/sachac">Follow me on Twitter</a> for quick updates, and add me on 
-<a href="https://plus.google.com/113865527017476906160/posts?rel=author" rel="publisher">Google+</a>. <a href="http://sachachua.com/blog/contact">Contact me</a>: 
-I'd love to hear your thoughts, questions, and suggestions!</p>
-  <br />
-  <ul class="links">
-    <li><a href="/blog">Home</a></li>
-    <li><a href="/blog/about/">About</a></li>
-    <li><a href="http://dl.dropbox.com/u/3968124/blog.html">Index</a></li>
-    <li><a href="/blog/archive/">Archives</a></li>
-    <li><a href="/blog/?random">Random</a></li>
-    <li><a href="/blog/privacy">Privacy</a>
-    <li><a href="/blog/contact">Contact</a>
-  </li></ul>
-
-
-  <p>
-    Copyright &#169; 2001-2014 Sacha Chua (<a href="mailto:sacha@sachachua.com">sacha@sachachua.com</a>). Please feel free to reuse or share content under a <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution</a> license unless otherwise noted. You can generally click on images for larger versions. Thanks for reading!
-  </p>
-</div>
-
-  <script>
-    $(document).foundation();
-    $("img#site-logo").hover(function() { 
-      $(this).attr("src", "<?php bloginfo('stylesheet_directory'); ?>/images/animated-logo.gif");
-    }, function() { 
-      $(this).attr("src", "<?php bloginfo('stylesheet_directory'); ?>/images/small-logo.png");
-    }); 
-  </script>
-<script src="http://www.google.com/jsapi" type="text/javascript"></script>
-<?php wp_footer(); ?>
-</div>
+    <script>
+      $(document).foundation();
+      $("img#site-logo").hover(function() { 
+        $(this).attr("src", "<?php bloginfo('stylesheet_directory'); ?>/images/animated-logo.gif");
+      }, function() { 
+        $(this).attr("src", "<?php bloginfo('stylesheet_directory'); ?>/images/small-logo.png");
+      }); 
+    </script>
+  <script src="http://www.google.com/jsapi" type="text/javascript"></script>
+  <?php wp_footer(); ?>
+  </div>
 </footer>
 </body>
 </html>
